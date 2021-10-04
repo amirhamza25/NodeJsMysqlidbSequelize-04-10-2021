570d20bee39c3adb06f708e3ccb9351eb58b2d93
Amir Hamza <amir.hamza251192@gmail.com>
2021-10-04 13:07:25 +0600

    first commit

---
+++ models/Posts.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+    const Posts = sequelize.define("Posts", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      postText: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      sopnsorid: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+
+    return Posts;
+  };
